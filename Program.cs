c8e60c3f7e86612f4c3d9f9a4f16f831302f8587
Jason Chesters-salt <43147625+jasonTheHuman2101@users.noreply.github.com>
2022-11-27 17:40:05 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DijkstraAlgorithmV2 //Dij-kstrer
+{
+    internal class Program
+    {
+        static string startNode;
+        static string endNode;
+
+        static void Main(string[] args)
+        {
+            //Takes in the start and end node names
+            Console.Write("Enter start node:\n>> ");
+            startNode = Console.ReadLine();
+            Console.Write("Enter end node:\n>> ");
+            endNode = Console.ReadLine();
+        }
+    }
+}
