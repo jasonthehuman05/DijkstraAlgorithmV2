7a3041bdc4a393ca8a6a8f8e39ea61048e5b467f
Jason Chesters-salt <43147625+jasonTheHuman2101@users.noreply.github.com>
2022-11-27 18:02:11 +0000

    added loading of the graph

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace DijkstraAlgorithmV2 //Dij-kstrer
 {
     internal class Program
     {
+        static Dictionary<string, Node> nodeDictionary = new Dictionary<string, Node>(); //Contains all the nodes
+        static HashSet<string> unvisitedNodes = new HashSet<string>(); //Contains any unvisited nodes. HashSets don't allow duplicates
+        static List<Route> routes = new List<Route>();
+
         static string startNode;
         static string endNode;
 
@@ -14,6 +20,34 @@ namespace DijkstraAlgorithmV2 //Dij-kstrer
             startNode = Console.ReadLine();
             Console.Write("Enter end node:\n>> ");
             endNode = Console.ReadLine();
+        }
+
+        static void LoadGraph()
+        {
+            if (!File.Exists("graph.csv"))
+            {
+                throw new FileNotFoundException("File not found");
+            }
+
+            //Load graph file
+            string[] file = File.ReadAllLines("graph.csv");
+            foreach(string s in file)
+            {
+                //Converts string into the contained parameters (from, to, cost)
+                string[] param = s.Split(", ");
+                var (from, to, dist) = (param[0], param[1], double.Parse(param[2]));
+
+                //Adds any non-existing nodes to the dict
+                if (nodeDictionary.ContainsKey(from)){ nodeDictionary.Add(from, new Node(from)); }
+                if (nodeDictionary.ContainsKey(to)){ nodeDictionary.Add(to, new Node(to)); }
+
+                //Adds the nodes to the HashTable as unvisited nodes. Duplicates are ignored
+                unvisitedNodes.Add(from);
+                unvisitedNodes.Add(to);
+
+                //Add the route to the list of possible routes
+                routes.Add(new Route(from, to, dist));
+            }
         }
     }
 }
---
+++ Route.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DijkstraAlgorithmV2
+{
+    internal class Route
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public double Distance { get; set; }
+
+        public Route(string frm, string _to, double dst)
+        {
+            From = frm;
+            To = _to;
+            Distance = dst;
+        }
+    }
+}
